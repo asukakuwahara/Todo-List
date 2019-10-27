70af6851d38c5ebffa8348f59c2e3204a71d53ca
asucarlos <kuwahara.mee@gmail.com>
2019-10-27 19:23:39 -0400

    add addtodo function

--- src/App.js
+++ src/App.js
@@ -7,15 +7,36 @@ import Todos from "./components/Todos";
 function App() {
   const [todos, setTodos] = useState([
     {
+      date: Date.now(),
       title: "walk my dog",
       desc: "for 30min",
       done: false,
       due: "2019 - 10 - 27"
     }
   ]);
+  const [error, setError] = useState(false);
+
+  const addTodo = (title, desc, due) => {
+    let currentDate = Date.now();
+    const newTodo = { title, desc, done: false, due, date: currentDate };
+    setTodos([...todos, newTodo]);
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+    const title = e.target.elements.title.value;
+    const desc = e.target.elements.desc.value;
+    const due = e.target.elements.due.value;
+    if ((title, desc, due)) {
+      addTodo(title, desc, due);
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
   return (
     <div className="App">
-      <Todos todos={todos} />
+      <Todos todos={todos} submitHandler={submitHandler} error={error} />
     </div>
   );
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../styles/Todos.css";
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, submitHandler, error }) => {
   const todoList = todos.length ? (
     todos.map(todo => {
       return (
@@ -17,7 +17,20 @@ const Todos = ({ todos }) => {
   ) : (
     <span>Nothing to do</span>
   );
-  return <div>{todoList}</div>;
+  return (
+    <div>
+      <form onSubmit={e => submitHandler(e)} className="todo-form">
+        <input type="text" name="title" placeholder="Type todo" />
+        <input type="text" name="desc" placeholder="Type description" />
+        <input type="date" name="due" />
+        <button type="submit" className="submit">
+          Add todo
+        </button>
+      </form>
+      {error && <p className="error">Fill out all the form</p>}
+      <div>{todoList}</div>
+    </div>
+  );
 };
 
 export default Todos;

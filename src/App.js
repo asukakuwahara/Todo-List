36558fe51f758395fc7f611c97ca7298f68d4320
asucarlos <kuwahara.mee@gmail.com>
2019-10-27 19:21:12 -0400

    show todos on screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/App.css";
 
 // screens
 import Todos from "./components/Todos";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      title: "walk my dog",
+      desc: "for 30min",
+      done: false,
+      due: "2019 - 10 - 27"
+    }
+  ]);
   return (
     <div className="App">
-      <Todos />
+      <Todos todos={todos} />
     </div>
   );
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
 import "../styles/Todos.css";
 
-const Todos = () => {
-  return <div>this is todos</div>;
+const Todos = ({ todos }) => {
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="todo-item" key={todo.id}>
+          <button>X</button>
+          <p>{todo.title}</p>
+          <p>{todo.desc}</p>
+          <p>Status: {todo.done ? <>Done!</> : <>Pending</>}</p>
+          <p>{todo.due}</p>
+        </div>
+      );
+    })
+  ) : (
+    <span>Nothing to do</span>
+  );
+  return <div>{todoList}</div>;
 };
 
 export default Todos;
